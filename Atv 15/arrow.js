997de40478b80ee3d228ce802c3187b00db75455
badzin23 <160626398+badzin23@users.noreply.github.com>
2024-06-04 12:41:23 -0300

    Add files via upload

---
+++ Atv 15/arrow.js
@@ -0,0 +1,20 @@
+
+//cria um array de numeros definindo seus valores
+//map vai percorrer todos os valores do array, e depois disso vai dobrar cada elemento dentro do array
+//atv1//
+function arrow1(){
+    const dobro = [3,7,1,9,5]; 
+    let resultado = dobro.map((dobro) => dobro * 2); 
+    console.log(resultado)
+};
+
+//atv2//
+function arrow2(){
+
+};
+
+//atv3//
+function arrow3(){
+
+};
+
