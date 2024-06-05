05df88f7eecd914a3dcc507b968da1506a2fbb46
badzin23 <160626398+badzin23@users.noreply.github.com>
2024-06-04 22:33:04 -0300

    Update arrow.js

--- Atv 15/arrow.js
+++ Atv 15/arrow.js
@@ -1,3 +1,5 @@
+//arrow de forma resumida, é uma forma mais enxuta de escrever funções no javascript utilizando =>
+
 
 //cria um array de numeros definindo seus valores
 //map vai percorrer todos os valores do array, e depois disso vai dobrar cada elemento dentro do array
@@ -8,13 +10,19 @@ function arrow1(){
     console.log(resultado)
 };
 
+//cria um array de palavras definindo cada palavra
+//ordenar.sort ira ordenar os elementos que estão na array ja criada, e então irá comparar o primeiro elemento com o segundo pelo seu tamanho e assim por diante
+//assim retornando as palavras na ordem da mais curta para a mais longa
 //atv2//
 function arrow2(){
-
+    const ordenar = ["Gato", "Tigre", "Cão", "Elefante"];
+    ordenar.sort((a, b) => a.length - b.length);
+    console.log(ordenar)
 };
 
 //atv3//
 function arrow3(){
-
+    const pares = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let result = pares.filter(pares => pares / 2);
 };
 
